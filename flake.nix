87cbfd720b45e2deadaa2d2f73946b9a6fbae8b9
giannifer7 <gianni.ferrarotti@gmail.com>
2026-03-15 13:37:19 +0100

    chore: release v0.1.1

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
   outputs = { self, nixpkgs }:
     let
       pkgs    = nixpkgs.legacyPackages.x86_64-linux;
-      version = "0.1.0";
+      version = "0.1.1";
       base    = "https://github.com/giannifer7/azadi/releases/download/v${version}";
-      fetch   = filename: sha256: pkgs.fetchurl { url = "${base}/${filename}"; inherit sha256; };
     in {
-      packages.x86_64-linux.default = pkgs.runCommand "azadi-${version}" {} ''
-        mkdir -p $out/bin
-        install -m755 ${fetch "azadi-musl"        "sha256-PLACEHOLDER"} $out/bin/azadi
-        install -m755 ${fetch "azadi-macros-musl" "sha256-PLACEHOLDER"} $out/bin/azadi-macros
-        install -m755 ${fetch "azadi-noweb-musl"  "sha256-PLACEHOLDER"} $out/bin/azadi-noweb
-      '';
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        pname   = "azadi";
+        inherit version;
+        src     = pkgs.fetchurl { url = "${base}/azadi-musl"; sha256 = "sha256-r5zpprnREu5rZ/OBToP+h32fO/PlnFvabct+a8+e+FE="; };
+        dontUnpack   = true;
+        installPhase = "install -Dm755 $src $out/bin/azadi";
+      };
     };
 }
